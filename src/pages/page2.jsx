0e7d47d290f3dd47e068442f855f0fd7e961bfd3
lilianaruva <lily.gallegos@hotmail.com>
2022-06-07 21:53:28 -0500

    left section design

--- src/pages/page2.jsx
+++ src/pages/page2.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React from "react";
 
 const Page2 = () => {
-    return (
+  return (
+    <>
       <>
-        <h2>Page2</h2>
+        <div className="page-2">
+          <div className="sectionLeft">
+            <div className="titlePage2">
+              <h1>
+                <span>Save more with Bellotero.io</span>
+              </h1>
+            </div>
+            <div className="reviewOverride">
+              With Bellotero.io you save time and money make real-time decisions
+              that boost your business and your bottom line. Get less wrongfully
+              blocked payments, save time on bookkeeping and no need to worry
+              about safety.{" "}
+            </div>
+          </div>
+          <div className="section">
+            <div className="row">hola</div>
+            <div className="row"></div>
+            <div className="row">
+              <div className="col"></div>
+              <div className="col"></div>
+            </div>
+          </div>
+        </div>
       </>
-    );
-  };
-  
-  export default Page2;
-  
+    </>
+  );
+};
+
+export default Page2;
